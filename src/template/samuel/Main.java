75038dee7eac750bfec1e7e8c857c7ded5932b89
Samuel Luo <sam@hospo360.com>
2021-01-08 15:14:50 +1300

    init project

---
+++ src/template/samuel/Main.java
@@ -0,0 +1,9 @@
+package template.samuel;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("i am the best thing ever!!!!!");
+    }
+}
